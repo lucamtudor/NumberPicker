71969590006830d5d81c3a710f5443a14c012bb4
Tudor Luca <luca.mtudor@gmail.com>
2014-09-11 23:07:22 +0300

    Initial commit.

---
+++ sample/src/main/java/ro/tudorluca/numberpicker/sample/MainActivity.java
@@ -0,0 +1,49 @@
+package ro.tudorluca.numberpicker.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import ro.tudorluca.numberpicker.NumberPicker;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NumberPicker picker = (NumberPicker) findViewById(R.id.picker);
+        picker.setMinValue(0);
+        picker.setMaxValue(10);
+        picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                Toast.makeText(MainActivity.this, "Value: " + newVal, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
